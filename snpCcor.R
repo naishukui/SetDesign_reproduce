a0f68a483807314181116136284b6fd5aee01906
Naishu Kui <40501750+naishukui@users.noreply.github.com>
2025-06-20 11:36:26 -0500

    Update snpCcor.R

--- snpCcor.R
+++ snpCcor.R
@@ -1,4 +1,4 @@
-# For continuous outcome and correlqted genotypes, simulate the power for true and misspecified  models using SKAT,
+# For continuous outcome and correlated genotypes, simulate the power for true and misspecified  models using SKAT,
 # and also calculate the analytical power using  Lee's method
 
 library(SKAT)
@@ -10,18 +10,19 @@ output_file_name<-"snpCcor_"
 index <- as.numeric(commandArgs(TRUE)[1])
 
 beta1list <- list(
+  c(-0.5, -0.4, -0.3, -0.2, -0.1, -0.5, -0.4, -0.3, -0.2, -0.1,
+    -0.5, -0.4, -0.3, -0.2, -0.1, -0.5, -0.4, -0.3, -0.2, -0.1,
+    -0.5, -0.4, -0.3, -0.2, -0.1),
   c(-0.9, -0.75, -0.5, -0.25, -0.1, -0.9, -0.75, -0.5, -0.25, -0.1,
     -0.9, -0.75, -0.5, -0.25, -0.1, -0.9, -0.75, -0.5, -0.25, -0.1,
     -0.9, -0.75, -0.5, -0.25, -0.1),
   c(-1.5, -1.25, -1, -0.75, -0.5, -1.5, -1.25, -1, -0.75, -0.5,
     -1.5, -1.25, -1, -0.75, -0.5, -1.5, -1.25, -1, -0.75, -0.5,
-    -1.5, -1.25, -1, -0.75, -0.5),
-  c(-3, -2.5, -2, -1.5, -1, -3, -2.5, -2, -1.5, -1,
-    -3, -2.5, -2, -1.5, -1, -3, -2.5, -2, -1.5, -1,
-    -3, -2.5, -2, -1.5, -1)
+    -1.5, -1.25, -1, -0.75, -0.5)
 )
 
 beta2list <- list(
+  c(rep(0.5, 5), rep(0.4, 5),  rep(0.3, 5), rep(0.2, 5),  rep(0.1, 5)),
   c(rep(0.9, 5), rep(0.75, 5), rep(0.5, 5), rep(0.25, 5), rep(0.1, 5)),
   c(rep(1.5, 5), rep(1.25, 5), rep(1, 5), rep(0.75, 5), rep(0.5, 5)),
   c(rep(3, 5), rep(2.5, 5), rep(2, 5), rep(1.5, 5), rep(1, 5))
